2a15fe5bcf37e796e2c2e38bdb8436001e94c46c
Carmelo Arato <carmelo@beeapp.it>
2025-10-05 17:01:44 +0200

    Improved script to add custom fields

--- script/addFields.sh
+++ script/addFields.sh
@@ -1,240 +1,208 @@
 #!/bin/bash
 
-moduleName=$1
-moduleNameLower=$(echo "$moduleName" | tr '[:upper:]' '[:lower:]')
-firstChar=$(echo "${moduleNameLower:0:1}" | tr '[:lower:]' '[:upper:]')
-rest="${moduleNameLower:1}"
-moduleNamePascalCase="$firstChar$rest"
+# ==============================================================================
+# Funzioni di Utilità
+# ==============================================================================
 
+# Funzione per convertire una stringa snake_case in lowerCase
+to_lower() {
+  echo "$1" | tr '[:upper:]' '[:lower:]'
+}
+
+# Funzione per convertire una stringa snake_case in upperCase
+to_upper() {
+  echo "$1" | tr '[:lower:]' '[:upper:]'
+}
+
+# Funzione per convertire una stringa snake_case in camelCase
+to_camel_case() {
+    echo "$1" | awk -F_ '{printf "%s", $1; for(i=2; i<=NF; i++) {printf "%s%s", toupper(substr($i,1,1)), substr($i,2)}}'
+}
+
+# Funzione per convertire una stringa in PascalCase (prima lettera maiuscola)
+to_pascal_case() {
+    local str="$1"
+    local first_char
+    first_char=$(to_upper "${str:0:1}")
+    echo "$first_char${str:1}"
+}
+
+# Funzione per inserire testo in un file usando un placeholder
+# Argomenti: 1=Placeholder, 2=Testo da inserire, 3=File di destinazione
+insert_code() {
+    local placeholder="$1"
+    local text_to_insert="$2"
+    local target_file="$3"
+    # Usa la sintassi su una sola riga, più robusta contro i problemi di indentazione
+    sed -i '' "/\/\/${placeholder}/ i\\
+${text_to_insert}
+" "${target_file}"
+}
+
+# Funzione per chiedere conferma all'utente (Y/n)
+confirm() {
+    local question="$1"
+    local response
+    read -r -p "$question (Y/n): " response
+    # Converte la risposta in minuscolo per un controllo più semplice
+    [[ "$(to_lower $response)" =~ ^(y|yes)$ ]]
+}
+
+# ==============================================================================
+# Inizializzazione e Setup
+# ==============================================================================
+
+# Controlla se il nome del modulo è stato fornito
+if [[ -z "$1" ]]; then
+    echo "Errore: Nessun nome del modulo fornito."
+    echo "Uso: $0 <NomeModulo>"
+    exit 1
+fi
+
+# Conversione dei nomi
+moduleNameLower=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+moduleNamePascalCase=$(to_pascal_case "$moduleNameLower")
+
+# Definizione dei nomi dei file
 daoFileName="dao/${moduleNameLower}_dao.dart"
 modelDaoFileName="dao/${moduleNameLower}_dao_model.dart"
 modelUpdateDaoFileName="dao/update_${moduleNameLower}_dao_model.dart"
 modelApiRequestFileName="api_request_models/${moduleNameLower}_model.dart"
 modelApiRequestUpdateFileName="api_request_models/update_${moduleNameLower}_model.dart"
 
-read -r -p "Do you want to add a custom fields this version? (Y/n): " readResult
-if [[ $readResult == [Yy] || $readResult == [yY][eE][sS] ]]
-then
+# ==============================================================================
+# Logica Principale
+# ==============================================================================
 
-  while true; do
-
-    read -r -p "Enter field name (ex. custom_field): " fieldName
-    if [[ fieldName == "" ]]
-    then
-      break
-    fi
-
-    fieldNameCamelCaseString=$(echo "$fieldName" | awk -F_ '{printf "%s", $1; for(i=2; i<=NF; i++) {printf "%s%s", toupper(substr($i,1,1)), substr($i,2)}; printf "\n"}')
-
-    columnRow="\${${moduleNamePascalCase}DaoModel.${fieldNameCamelCaseString}Key}"
-
-    read -r -p "Enter field SQL type (supported types: TEXT, INTEGER, TIMESTAMP): " fieldType
-    if [[ fieldType == "" || ($fieldType != "TEXT" && $fieldType != "INTEGER" && $fieldType != "TIMESTAMP") ]]
-    then
-      break
-    fi
-
-    columnRow="$columnRow $fieldType"
-    if [[ $fieldType == "TIMESTAMP" ]]
-    then
-      columnRow="$columnRow DATETIME"
-    fi
-
-    isNullable=1
-    read -r -p "Has it to be nullable?: (Y/n) " shouldNullable
-    if [[ $shouldNullable == "" ]]
-    then
-      break
-    fi
-
-    if [[ $shouldNullable == [Nn] || $shouldNullable == [Nn][Oo] ]]
-    then
-      isNullable=0
-      columnRow="${columnRow} NOT NULL"
-    fi
-
-    hasDefault=0
-    read -r -p "Has it a default value?: (Y/n) " shouldHasDefault
-    if [[ $shouldHasDefault == "" ]]
-    then
-      break
-    fi
-
-    if [[ $shouldHasDefault == [Yy] || $shouldHasDefault == [yY][eE][sS] ]]
-    then
-      hasDefault=1
-      read -r -p "Insert default value" defaultValue
-      columnRow="${columnRow} DEFAULT $defaultValue"
-    fi
-
-    read -r -p "Has it to be unique?: (Y/n) " shouldUnique
-    if [[ $shouldUnique == "" ]]
-    then
-      break
-    fi
-
-    if [[ $shouldUnique == [Yy] || $shouldUnique == [yY][eE][sS] ]]
-    then
-      updateRow="\"UNIQUE(\${${moduleNamePascalCase}DaoModel.${fieldNameCamelCaseString}Key})\","
-      l="/\/\/CONSTRAINT_PLACE_HOLDER"
-      sed -i '' "$l/i\\
-      $updateRow
-      " $daoFileName
-    fi
-
-    l="/\/\/COLUMN_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-    \"$columnRow\"
-    " $daoFileName
-
-    updateRow="${moduleNamePascalCase}DaoModel.${fieldNameCamelCaseString}Key,"
-    l="/\/\/COLUMN_NAME_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-    $updateRow
-    " $daoFileName
-
-    updateRow="${moduleNameLower}.${fieldNameCamelCaseString},"
-    l="/\/\/COLUMN_VALUE_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-    $updateRow
-    " $daoFileName
-
-    updateRow=$'\t\t'"if (${moduleNameLower}.${fieldNameCamelCaseString} != null) Update${moduleNamePascalCase}DaoModel.${fieldNameCamelCaseString}Key,"
-    l="/\/\/COLUMN_UPDATE_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-    $updateRow
-    " $daoFileName
-
-    updateRow=$'\t\t'"if (${moduleNameLower}.${fieldNameCamelCaseString} != null) ${moduleNameLower}.${fieldNameCamelCaseString},"
-    l="/\/\/COLUMN_UPDATE_VALUE_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-    $updateRow
-    " $daoFileName
-
-    row="static const String ${fieldNameCamelCaseString}Key = \"${fieldNameCamelCaseString}\";"
-    daoRow="static const String ${fieldNameCamelCaseString}Key = \"${fieldName}\";"
-    updateDaoRow="static const String ${fieldNameCamelCaseString}Key = ${moduleNamePascalCase}DaoModel.${fieldNameCamelCaseString}Key;"
-    updateRow="static const String ${fieldNameCamelCaseString}Key = ${moduleNamePascalCase}Model.${fieldNameCamelCaseString}Key;"
-    l="/\/\/KEY_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-  $daoRow
-    " $modelDaoFileName
-
-    l="/\/\/KEY_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-  $updateDaoRow
-    " $modelUpdateDaoFileName
-
-    l="/\/\/KEY_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-  $row
-    " $modelApiRequestFileName
-
-    l="/\/\/KEY_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-  $updateRow
-    " $modelApiRequestUpdateFileName
-
-    row="final"
-    updateRow="final"
-    if [[ $fieldType == "TEXT" ]]
-    then
-      dartFieldType="String"
-    elif [[ $fieldType == "INTEGER" ]]
-    then
-      dartFieldType="int"
-    elif [[ $fieldType == "TIMESTAMP" ]]
-    then
-      dartFieldType="DateTime"
-    fi
-
-    row="${row} $dartFieldType"
-    updateRow="${row}?"
-
-    if [[ $isNullable == 1 ]]
-    then
-      row="${row}?"
-    fi
-
-    row="${row} ${fieldNameCamelCaseString};"
-    updateRow="${updateRow} ${fieldNameCamelCaseString};"
-    l="/\/\/FIELD_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-    $row
-    " $modelDaoFileName
-
-    l="/\/\/FIELD_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-    $updateRow
-    " $modelUpdateDaoFileName
-
-    l="/\/\/FIELD_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-  $row
-    " $modelApiRequestFileName
-
-    l="/\/\/FIELD_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-  $updateRow
-    " $modelApiRequestUpdateFileName
-
-    row="required this.${fieldNameCamelCaseString},"
-    l="/\/\/CONSTRUCTOR_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-    $row
-    " $modelDaoFileName
-
-    l="/\/\/CONSTRUCTOR_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-    $row
-    " $modelUpdateDaoFileName
-
-    constructorRow="$fieldNameCamelCaseString = data[${fieldNameCamelCaseString}Key],"
-    l="/\/\/CONSTRUCTOR_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-    $constructorRow
-    " $modelApiRequestFileName
-
-    l="/\/\/CONSTRUCTOR_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-    $constructorRow
-    " $modelApiRequestUpdateFileName
-
-    if [[ $isNullable == 0 && $hasDefault == 0 ]]
-    then
-      requiredRow="${fieldNameCamelCaseString}Key,"
-      l="/\/\/REQUIRED_FIELD_PLACE_HOLDER"
-      sed -i '' "$l/i\\
-    $requiredRow
-      " $modelApiRequestFileName
-    fi
-
-    fieldTypeRow="${fieldNameCamelCaseString}Key: ${dartFieldType},"
-    l="/\/\/FIELD_TYPE_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-    $fieldTypeRow
-    " $modelApiRequestFileName
-
-    toDaoRow="$fieldNameCamelCaseString: $fieldNameCamelCaseString,"
-    l="/\/\/TO_DAO_PLACE_HOLDER"
-    sed -i '' "$l/i\\
-    $toDaoRow
-    " $modelApiRequestFileName
-
-    sed -i '' "$l/i\\
-    $toDaoRow
-    " $modelApiRequestUpdateFileName
-
-    read -r -p "Do you want to add another field? (Y/n): " readResult
-
-    if [[ $readResult == [Nn] || $readResult == [Nn][Oo] ]]
-    then
-      lastConstructorString="${constructorRow%,}"
-      searchString=$(echo "$constructorRow" | sed -e 's/\[/\\[/g' -e 's/\]/\\]/g')
-
-      sed -i '' "s/${searchString}/${lastConstructorString}/g" "$modelApiRequestFileName"
-      sed -i '' "s/${searchString}/${lastConstructorString}/g" "$modelApiRequestUpdateFileName"
-      break
-    fi
-  done
+if ! confirm "Vuoi aggiungere campi personalizzati a questo modulo?"; then
+    echo "Operazione annullata."
+    exit 0
 fi
+
+while true; do
+
+    # --- Raccolta Input Utente ---
+    read -r -p "Inserisci il nome del campo (es. custom_field, lascia vuoto per uscire): " fieldName
+    if [[ -z "$fieldName" ]]; then
+        echo "Nessun campo inserito. Uscita dal ciclo."
+        break
+    fi
+
+    fieldNameCamelCase=$(to_camel_case "$fieldName")
+
+    read -r -p "Inserisci il tipo SQL (TEXT, INTEGER, TIMESTAMP): " fieldType
+    # Converte in maiuscolo per rendere il controllo case-insensitive
+    fieldTypeUpper=$(echo "$fieldType" | tr '[:lower:]' '[:upper:]')
+    case "$fieldTypeUpper" in
+        "TEXT"|"INTEGER"|"TIMESTAMP")
+            # Tipo valido, continua
+            ;;
+        *)
+            echo "Tipo non valido. Uscita dal ciclo."
+            break
+            ;;
+    esac
+
+    isNullable=true
+    if ! confirm "Deve essere nullable?"; then
+        isNullable=false
+    fi
+
+    hasDefault=false
+    defaultValue=""
+    if confirm "Ha un valore di default?"; then
+        hasDefault=true
+        read -r -p "Inserisci il valore di default: " defaultValue
+    fi
+
+    isUnique=false
+    if confirm "Deve essere unico?"; then
+        isUnique=true
+    fi
+
+
+    # --- Preparazione delle Stringhe da Inserire ---
+
+    # SQL Column Definition
+    columnRow="\${${moduleNamePascalCase}DaoModel.${fieldNameCamelCase}Key} $fieldTypeUpper"
+    if [[ "$fieldTypeUpper" == "TIMESTAMP" ]]; then
+        columnRow+=" DATETIME"
+    fi
+    if ! $isNullable; then
+        columnRow+=" NOT NULL"
+    fi
+    if $hasDefault; then
+        # Aggiunge apici per i valori di testo di default
+        if [[ "$fieldTypeUpper" == "TEXT" || "$fieldTypeUpper" == "TIMESTAMP" ]]; then
+            columnRow+=" DEFAULT '$defaultValue'"
+        else
+            columnRow+=" DEFAULT $defaultValue"
+        fi
+    fi
+
+    # Dart Type
+    case "$fieldTypeUpper" in
+        "TEXT") dartFieldType="String" ;;
+        "INTEGER") dartFieldType="int" ;;
+        "TIMESTAMP") dartFieldType="DateTime" ;;
+    esac
+
+
+    # --- Inserimento del codice nei file ---
+
+    # File: dao/${moduleNameLower}_dao.dart
+    if $isUnique; then
+        insert_code "CONSTRAINT_PLACE_HOLDER" "\"UNIQUE(\${${moduleNamePascalCase}DaoModel.${fieldNameCamelCase}Key})\"," "$daoFileName"
+    fi
+    insert_code "COLUMN_PLACE_HOLDER" "    \"$columnRow\"," "$daoFileName"
+    insert_code "COLUMN_NAME_PLACE_HOLDER" "    ${moduleNamePascalCase}DaoModel.${fieldNameCamelCase}Key," "$daoFileName"
+    insert_code "COLUMN_VALUE_PLACE_HOLDER" "    ${moduleNameLower}.${fieldNameCamelCase}," "$daoFileName"
+    insert_code "COLUMN_UPDATE_PLACE_HOLDER" "        if (${moduleNameLower}.${fieldNameCamelCase} != null) Update${moduleNamePascalCase}DaoModel.${fieldNameCamelCase}Key," "$daoFileName"
+    insert_code "COLUMN_UPDATE_VALUE_PLACE_HOLDER" "        if (${moduleNameLower}.${fieldNameCamelCase} != null) ${moduleNameLower}.${fieldNameCamelCase}," "$daoFileName"
+
+    # File: dao/${moduleNameLower}_dao_model.dart
+    insert_code "KEY_PLACE_HOLDER" "  static const String ${fieldNameCamelCase}Key = \"${fieldName}\";" "$modelDaoFileName"
+    fieldDefinition="final $dartFieldType"
+    [[ $isNullable = true ]] && fieldDefinition+="?"
+    fieldDefinition+=" ${fieldNameCamelCase};"
+    insert_code "FIELD_PLACE_HOLDER" "  $fieldDefinition" "$modelDaoFileName"
+    insert_code "CONSTRUCTOR_PLACE_HOLDER" "    required this.${fieldNameCamelCase}," "$modelDaoFileName"
+
+    # File: dao/update_${moduleNameLower}_dao_model.dart
+    insert_code "KEY_PLACE_HOLDER" "  static const String ${fieldNameCamelCase}Key = ${moduleNamePascalCase}DaoModel.${fieldNameCamelCase}Key;" "$modelUpdateDaoFileName"
+    insert_code "FIELD_PLACE_HOLDER" "  final $dartFieldType? ${fieldNameCamelCase};" "$modelUpdateDaoFileName"
+    insert_code "CONSTRUCTOR_PLACE_HOLDER" "    required this.${fieldNameCamelCase}," "$modelUpdateDaoFileName"
+
+    # File: api_request_models/${moduleNameLower}_model.dart
+    insert_code "KEY_PLACE_HOLDER" "  static const String ${fieldNameCamelCase}Key = \"${fieldNameCamelCase}\";" "$modelApiRequestFileName"
+    insert_code "FIELD_PLACE_HOLDER" "  $fieldDefinition" "$modelApiRequestFileName"
+    constructorRow="$fieldNameCamelCase = data[${fieldNameCamelCase}Key],"
+    insert_code "CONSTRUCTOR_PLACE_HOLDER" "    $constructorRow" "$modelApiRequestFileName"
+    if ! $isNullable && ! $hasDefault; then
+        insert_code "REQUIRED_FIELD_PLACE_HOLDER" "    ${fieldNameCamelCase}Key," "$modelApiRequestFileName"
+    fi
+    insert_code "FIELD_TYPE_PLACE_HOLDER" "    ${fieldNameCamelCase}Key: ${dartFieldType}," "$modelApiRequestFileName"
+    toDaoRow="$fieldNameCamelCase: $fieldNameCamelCase,"
+    insert_code "TO_DAO_PLACE_HOLDER" "    $toDaoRow" "$modelApiRequestFileName"
+
+    # File: api_request_models/update_${moduleNameLower}_model.dart
+    insert_code "KEY_PLACE_HOLDER" "  static const String ${fieldNameCamelCase}Key = ${moduleNamePascalCase}Model.${fieldNameCamelCase}Key;" "$modelApiRequestUpdateFileName"
+    insert_code "FIELD_PLACE_HOLDER" "  final $dartFieldType? ${fieldNameCamelCase};" "$modelApiRequestUpdateFileName"
+    insert_code "CONSTRUCTOR_PLACE_HOLDER" "    $constructorRow" "$modelApiRequestUpdateFileName"
+    insert_code "TO_DAO_PLACE_HOLDER" "    $toDaoRow" "$modelApiRequestUpdateFileName"
+
+
+    # --- Fine del ciclo ---
+    if ! confirm "Vuoi aggiungere un altro campo?"; then
+        # Rimuove la virgola finale dall'ultima riga del costruttore nei file API
+        lastConstructorString="${constructorRow%,}"
+        # Sanifica la stringa per `sed` (escape di '[' e ']')
+        searchString=$(echo "$constructorRow" | sed -e 's/\[/\\[/g' -e 's/\]/\\]/g')
+
+        sed -i '' "s/${searchString}/${lastConstructorString}/g" "$modelApiRequestFileName"
+        sed -i '' "s/${searchString}/${lastConstructorString}/g" "$modelApiRequestUpdateFileName"
+        break
+    fi
+done
+
+echo "Operazione completata."
